87d6dd26486fc8787236c634bb9b437612533894
mrlex622 <mrlex622@gmail.com>
2026-06-26 11:37:15 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,48 @@
+{
+  "name": "parelex",
+  "version": "1.0.0",
+  "dependencies": {
+    "react": "^18.0.0",
+    "react-dom": "^18.0.0",
+    "react-scripts": "5.0.1"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build"
+  }
+}<!DOCTYPE html>
+<html>
+  <head>
+    <title>PARELEX</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PARELEX</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>{
+  "name": "parelex",
+  "version": "1.0.0",
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-scripts": "5.0.1"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build"
+  },
+  "browserslist": {
+    "production": [">0.2%"],
+    "development": ["last 1 chrome version"]
+  }
+  }
